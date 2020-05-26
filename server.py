60aa409a7e972527c027c5edddc3c80f871390b1
Mateus Souza <mateus_msouza@outlook.com>
2020-05-25 22:58:27 -0300

    add controller created to server routes

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
-
+from controller.credit_analyzer import CreditAnalyzer
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
+async def analyzer_route(request):
+    body = await request.json()
+    credit_controller = CreditAnalyzer(body["customer"])
+    return JSONResponse({
+        "credit_granted": credit_controller.grant_credit(),
+        "value": credit_controller.credit_granted()
+    })
 
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
+    Route('/analyze', analyzer_route, methods=['POST'])
 ])
