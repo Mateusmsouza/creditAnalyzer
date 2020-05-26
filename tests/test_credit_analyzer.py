ea3a247ba216067b525e7fdc9bbff2bd24b558d6
Mateus Souza <mateus_msouza@outlook.com>
2020-05-25 22:51:46 -0300

    correct name tests
    did too much copy paste on testing's name; fixing it

--- tests/test_credit_analyzer.py
+++ tests/test_credit_analyzer.py
@@ -10,19 +10,19 @@ def test_score_299_should_not_grant_credit():
     controller_credit_analyzer = CreditAnalyzer(customer)
     assert controller_credit_analyzer.grant_credit() is False
 
-def test_score_300_should_not_grant_credit():
+def test_score_300_should_grant_credit():
     customer = { "score": 300, "salary": 1000 }
     controller_credit_analyzer = CreditAnalyzer(customer)
     assert controller_credit_analyzer.grant_credit() is True
     assert controller_credit_analyzer.credit_granted() == 1000
 
-def test_score_599_should_not_grant_credit():
+def test_score_599_should_grant_credit():
     customer = { "score": 599, "salary": 1000 }
     controller_credit_analyzer = CreditAnalyzer(customer)
     assert controller_credit_analyzer.grant_credit() is True
     assert controller_credit_analyzer.credit_granted() == 1000
 
-def test_score_600_should_not_grant_credit():
+def test_score_600_should_grant_credit():
     customer = { "score": 600, "salary": 1000 }
     controller_credit_analyzer = CreditAnalyzer(customer)
     assert controller_credit_analyzer.grant_credit() is True
