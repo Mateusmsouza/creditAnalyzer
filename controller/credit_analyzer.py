b2be18f9eadeafbadb3309b6ea832869efd488d0
Mateus Souza <mateus_msouza@outlook.com>
2020-05-25 22:32:26 -0300

    make score 300 test case pass

--- controller/credit_analyzer.py
+++ controller/credit_analyzer.py
@@ -2,6 +2,16 @@ class CreditAnalyzer:
 
     def __init__(self, customer):
         self.customer = customer
+        self.credit = 0
 
     def grant_credit(self):
-        return False
+        score = self.customer["score"]
+
+        if score > 0 and score < 300:
+            return False
+        elif score > 299 and score < 600:
+            self.credit = 1000
+            return True
+            
+    def credit_granted(self):
+        return self.credit
